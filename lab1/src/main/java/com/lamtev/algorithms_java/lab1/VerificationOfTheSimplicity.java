0772d6c53293a6b8e478a88eae59aeba0e412a78
Anton Lamtev <antonlamtev@gmail.com>
2016-10-22 18:02:33 +0300

    add VerificationOfTheSimplicity

---
+++ lab1/src/main/java/com/lamtev/algorithms_java/lab1/VerificationOfTheSimplicity.java
@@ -0,0 +1,78 @@
+package com.lamtev.algorithms_java.lab1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class VerificationOfTheSimplicity {
+
+    private FastScanner in;
+    private PrintWriter out;
+
+    public static void main(String[] args) {
+        new VerificationOfTheSimplicity().run();
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner();
+            out = new PrintWriter(System.out);
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void solve() throws IOException {
+        out.println(isPrime(in.nextInt()) ? "True" : "False");
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 1)
+            return false;
+        for (int i = 2; i*i < number; ++i)
+            if (number % i == 0)
+                return false;
+        return true;
+    }
+
+    private class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        byte nextByte() {
+            return Byte.parseByte(next());
+        }
+
+        short nextShort() {
+            return Short.parseShort(next());
+        }
+
+    }
+
+}
